08fc89aa5736059144bcc1ed224000501359f4f0
Prashant Borole <prborole@microsoft.com>
2020-11-01 19:41:53 -0800

    display test

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,18 @@
 #include <DHT.h>
 #include <DHT_U.h>
 
-#define RELAY0_PIN 16   // D0
-#define RELAY1_PIN 13   // D7
-#define DHT11_PIN 14    // D5
-#define RCWL0516_PIN 12 // D6
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+
+#define RELAY0_PIN 16     // D0
+#define RELAY1_PIN 13     // D7
+#define DHT11_PIN 14      // D5
+#define RCWL0516_PIN 12   // D6
+#define SSD1306_SDA_PIN 4 // D2
+#define SSD1306_SCL_PIN 5 // D1
 
 // global vars
 
@@ -19,6 +27,10 @@ scheduler sched(8); // 'task' scheduler
 DHT_Unified dht_sensor(DHT11_PIN, DHT11); // DHT temp sensor (adafruit library)
 bool presence_detection_status = false;
 
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 
 void dht11_sensor_read_task(void *)
@@ -57,9 +69,6 @@ void setup_dht()
   sched.add_or_update_task((void *)&dht11_sensor_read_task, 0, NULL, 1, 2000, 0 /*5000*/);
 }
 
-
-
-
 void presence_detection_timeout_task(void *)
 {
   Serial.println(F("--- idle for too long"));
@@ -80,21 +89,66 @@ void setup_presence_detection()
   attachInterrupt(digitalPinToInterrupt(RCWL0516_PIN), presence_detection_task, RISING);
 }
 
-
-
 void setup()
 {
+  Serial.begin(9600);
+  // delay(2000);
+
+  delay(2000);
+  Serial.println(F("SSD1306 begin"));
+  Wire.begin(SSD1306_SDA_PIN, SSD1306_SCL_PIN);
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ; // Don't proceed, loop forever
+  }
+
+  /*
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+  */
+
+  display.clearDisplay();
+
+  while (true)
+  {
+    int box_size = 20;
+    for (size_t i = 0; i < 128; i += box_size)
+    {
+      for (size_t j = 0; j < 64; j += box_size)
+      {
+        // display.clearDisplay();
+
+        // Draw a single pixel in white
+        display.fillRect(i, j, box_size, box_size, SSD1306_WHITE);
+
+        // Show the display buffer on the screen. You MUST call display() after
+        // drawing commands to make them visible on screen!
+        display.display();
+        // delay(50);
+      }
+    }
+  }
+  // Draw a single pixel in white
+  display.drawPixel(10, 10, SSD1306_WHITE);
+
+  // Show the display buffer on the screen. You MUST call display() after
+  // drawing commands to make them visible on screen!
+  display.display();
+  delay(2000);
+  // Clear the buffer
+  display.clearDisplay();
 
   pinMode(RELAY0_PIN, OUTPUT);
   pinMode(RELAY1_PIN, OUTPUT);
 
-  Serial.begin(9600);
-  // delay(2000);
-
   setup_dht();
   setup_presence_detection();
 }
-
 
 void loop()
 {
