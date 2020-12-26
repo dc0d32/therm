e1b2f6a5503ebe2719f0a39320b6eab61c6893e5
Prashant Borole <boroleprashant@gmail.com>
2020-12-26 12:29:11 -0800

    enforce minimum running time for heat

--- src/control.cc
+++ src/control.cc
@@ -72,6 +72,7 @@ bool fan_on()
       digitalWrite(RELAY_FAN_PIN, HIGH);
       // Serial.println(String("fan turned on. scheduled Off task = ") + was_fan_off_task_added);
       bool was_fan_off_task_added = sched.add_or_update_task((void *)fan_off, 0, NULL, 0, 0, MS_FROM_MINUTES(45)); // safety task: fan can't run continuously for too long
+      last_fan_off_ts = -1;                                                                                        // fan is now on, no need to hold on to the last 'off' timestamp
       ret = true;
     }
   }
@@ -83,7 +84,7 @@ bool fan_on()
   return ret;
 }
 
-int64_t last_heat_off_ts = -1;
+int64_t last_heat_off_ts = -1, last_heat_on_ts = -1;
 bool heat_off()
 {
   // Serial.println("heat off called");
@@ -96,16 +97,24 @@ bool heat_off()
   }
   else
   {
-    // TODO: in case we want to enforce minimum running time for a heat, handle that here.
+    // enforce minimum running time for a heating session
+    if (last_heat_on_ts > 0 && millis() - last_heat_on_ts < MS_FROM_MINUTES(5))
+    {
+      // Serial.println(String("heating hasn't been running long enough. Not stopping."));
+      ret = false;
+    }
+    else
+    {
+      therm_state.heat_relay = 0;
+      digitalWrite(RELAY_HEAT_PIN, LOW);
+      bool was_heat_on_task_removed = sched.remove_task((void *)fan_on, 0);
+      bool was_heat_off_task_removed = sched.remove_task((void *)heat_off, 0);
+      // Serial.println(String("heat turned off. scheduled task removed status: fan on = ") + was_heat_on_task_removed + String(", heat off = ") + was_heat_off_task_removed);
 
-    therm_state.heat_relay = 0;
-    digitalWrite(RELAY_HEAT_PIN, LOW);
-    bool was_heat_on_task_removed = sched.remove_task((void *)fan_on, 0);
-    bool was_heat_off_task_removed = sched.remove_task((void *)heat_off, 0);
-    // Serial.println(String("heat turned off. scheduled task removed status: fan on = ") + was_heat_on_task_removed + String(", heat off = ") + was_heat_off_task_removed);
-
-    last_heat_off_ts = millis();
-    ret = true;
+      last_heat_off_ts = millis();
+      last_heat_on_ts = -1; // heat is now off, no need to hold on to the last 'on' timestamp
+      ret = true;
+    }
   }
   sched.add_or_update_task((void *)send_mqtt_state_relays, 0, NULL, 0, 0, 100);
   draw_icon_heat(therm_state.heat_relay);
@@ -136,10 +145,13 @@ bool heat_on()
     {
       therm_state.heat_relay = 1;
       digitalWrite(RELAY_HEAT_PIN, HIGH);
-      bool was_fan_on_task_added = sched.add_or_update_task((void *)fan_on, 0, NULL, 0, 0, MS_FROM_MINUTES(1));     // safety task: fan must come on few seconds after heat does, even if we don't hear anything from the controller
+      bool was_fan_on_task_added = sched.add_or_update_task((void *)fan_on, 0, NULL, 0, 0, MS_FROM_MINUTES(1));      // safety task: fan must come on few seconds after heat does, even if we don't hear anything from the controller
       bool was_heat_off_task_added = sched.add_or_update_task((void *)heat_off, 0, NULL, 0, 0, MS_FROM_MINUTES(30)); // safety task: heat can not run for for too long
 
       // Serial.println(String("heat turned on. scheduled task added: fan on = ") + was_fan_on_task_added + String(" , heat off = ") + was_heat_off_task_added);
+
+      last_heat_on_ts = millis();
+      last_heat_off_ts = -1; // heat is now on, no need to hold on to the last 'off' timestamp
 
       ret = true;
     }
